1fa0a2f979b0f23fe038613656668d8056bae723
Yan2020729 <68972722+Yan2020729@users.noreply.github.com>
2024-04-19 16:56:21 -0400

    Update ohal_nuisance_2ts.R

--- ohal_nuisance_2ts.R
+++ ohal_nuisance_2ts.R
@@ -14,15 +14,13 @@ ohal_nuisance_2ts <- function(L0, A0, L1, A1, L2, abar, V = 10, outcome_family =
 
   n <- length(L2)
   # make a vector of cv-folds
-
-  fold_idx <- chunk2(seq_len(n), V) # output V chunks: i.e. $`9` 801-900
-  folds <- rep(seq_len(V), unlist(lapply(fold_idx, length)))  # 1...1 (100 1s), 2...2 (100 2s),...
-  # fold_idx <- unlist(fold_idx, use.names = FALSE)  # 1,2,..., 1000
+  fold_idx <- chunk2(seq_len(n), V) # output V chunks
+  folds <- rep(seq_len(V), unlist(lapply(fold_idx, length)))  
 
 
   ################### outcome
   # cross-validation for OR
-  cv_out <- sapply(seq_len(V), one_hal_or_m2ts,  # use one_hal_or(), output $QAW, $riskQ (MSE), $Q1W, $Q0W, $or_coef (coefs from hal of Q)
+  cv_out <- sapply(seq_len(V), one_hal_or_m2ts,  
                    folds = folds, L0 = L0, A0 = A0, L1 = L1, A1 = A1, L2 = L2,
                    lambda_seq = lambda_seq, outcome_family = outcome_family, simplify = FALSE)
 
@@ -39,11 +37,8 @@ ohal_nuisance_2ts <- function(L0, A0, L1, A1, L2, abar, V = 10, outcome_family =
 
 
   # get cv selected OR coefficients for each fold
-  Q2mod_coef_list <- lapply(cv_out, function(x){ x$coefs$Q2mod[, lambda_or_idx[1]] })  # length(cv_out)=length(or_coef_list)=5, different length for each or_coef_list[[a]] for a = 1,...,5
+  Q2mod_coef_list <- lapply(cv_out, function(x){ x$coefs$Q2mod[, lambda_or_idx[1]] })  #
   Q1mod_coef_list <- lapply(cv_out, function(x){ x$coefs$Q1mod[[lambda_or_idx[1]]][, lambda_or_idx[2]] })
-  # sapply(1:5, function(x) summary(Q1mod_coef_list[[x]]))
-  # sapply(1:5, function(x) length(Q2mod_coef_list[[x]])) #  2387 2389 2386 2386 2387
-  # sapply(1:5, function(x) length(Q1mod_coef_list[[x]]))  # 801 801 801 801 801
 
 
   ################### PS
@@ -81,8 +76,6 @@ ohal_nuisance_2ts <- function(L0, A0, L1, A1, L2, abar, V = 10, outcome_family =
 #--------------------- helper functions ---------------------
 chunk2 <- function(x, n) split(x, cut(seq_along(x), n, labels = FALSE))
 
-
-
 esum_list <- function(list) {
   ll = length(list); suml = 0
   for (i in 1:ll){  suml = suml + as.matrix(list[[i]]) }
@@ -91,7 +84,6 @@ esum_list <- function(list) {
 
 
 # A helper function for fitting a single fold of HAL on outcome. Can be used both for fitting cross-validated HAL and fitting HAL to full data.
-
 one_hal_or_m2ts <- function(validFold, folds, L0, A0, L1, A1, L2, outcome_family = "binomial",
                             newdata = NULL, lambda_seq = exp(seq(0, -13, length=2000))){
   n <- length(L2)
@@ -112,8 +104,6 @@ one_hal_or_m2ts <- function(validFold, folds, L0, A0, L1, A1, L2, outcome_family
     valid_A0 <- A0[folds == validFold]; valid_A1 <- A1[folds == validFold]; valid_L2 <- L2[folds == validFold]
   }
 
-
-  # for outcome regression
   #--------
   # Q2n
   #--------
@@ -123,14 +113,14 @@ one_hal_or_m2ts <- function(validFold, folds, L0, A0, L1, A1, L2, outcome_family
   x2_fit = cbind(train_L0,train_L1)
   y2_fit = as.matrix(train_L2)
   new_x2 = cbind(L0,L1)
-  basis_list <- enumerate_basis(x2_fit, NULL) # length(basis_list): 9608
-  x2_basis <- hal9001:::make_design_matrix(x2_fit, basis_list)  # dim(x2_basis): 226 678
-  copy_map <- hal9001:::make_copy_map(x2_basis) # length(copy_map) 652, output a list of numeric vectors indicating indices of basis functions that are identical in the training set
-  unique_columns <- as.numeric(names(copy_map)) # length(unique_columns) 652
+  basis_list <- enumerate_basis(x2_fit, NULL) # 
+  x2_basis <- hal9001:::make_design_matrix(x2_fit, basis_list)  # 
+  copy_map <- hal9001:::make_copy_map(x2_basis) # 
+  unique_columns <- as.numeric(names(copy_map)) #
   # subset to non-duplicated columns
   x2_basis_fit <- x2_basis[, unique_columns]
   dim(x2_basis_fit)
-  x2_basis_fit = cbind(train_A0, train_A1, x2_basis_fit) # dim(x2_basis_fit)
+  x2_basis_fit = cbind(train_A0, train_A1, x2_basis_fit) # 
 
   ##### fit outcome regression
   hal_lasso2 <- glmnet::glmnet(x = x2_basis_fit, y = y2_fit, family = outcome_family, lambda = lambda_seq, standardize = FALSE)
@@ -151,8 +141,8 @@ one_hal_or_m2ts <- function(validFold, folds, L0, A0, L1, A1, L2, outcome_family
   new_x1 = as.matrix(valid_L0)
   basis_list <- enumerate_basis(x1_fit, NULL)
   x1_basis <- hal9001:::make_design_matrix(x1_fit, basis_list)  # dim: n_train * 445
-  copy_map <- hal9001:::make_copy_map(x1_basis) # length(copy_map) 445, output a list of numeric vectors indicating indices of basis functions that are identical in the training set
-  unique_columns <- as.numeric(names(copy_map)) # length(unique_columns) 445
+  copy_map <- hal9001:::make_copy_map(x1_basis) # 
+  unique_columns <- as.numeric(names(copy_map)) # 
   # subset to non-duplicated columns
   x1_basis_fit <- x1_basis[, unique_columns] # dim(x1_basis_fit)
   x1_basis_fit = cbind(train_A0, x1_basis_fit) # dim(x2_basis_fit)
@@ -160,7 +150,7 @@ one_hal_or_m2ts <- function(validFold, folds, L0, A0, L1, A1, L2, outcome_family
   risks_allamb = NULL
   or_pred_list1 = Q1mod_coef_list = list()
   new_x_basis1 <- hal9001:::make_design_matrix(new_x1, basis_list)
-  new_x_basis1 <- as.matrix(new_x_basis1[, unique_columns])  # dim(new_x_basis1): n_valid * 445
+  new_x_basis1 <- as.matrix(new_x_basis1[, unique_columns])  
 
   for (j in 1:length(lambda_seq)){
 
@@ -169,7 +159,7 @@ one_hal_or_m2ts <- function(validFold, folds, L0, A0, L1, A1, L2, outcome_family
     hal_lasso1 <- glmnet::glmnet(x = x1_basis_fit, y = y1_fit, lambda = lambda_seq, standardize = FALSE)
 
     or_pred_matrix1 <- cbind(rep(1, ifelse(is.null(validFold), n, n_valid)), cbind(A0=abar[1],new_x_basis1)) %*% rbind(hal_lasso1$a0, hal_lasso1$beta) # dim(or_pred_matrix2)  n * length(lambda_seq)
-    # if(outcome_family == "binomial"){ or_pred_matrix1 <- apply(or_pred_matrix1, 2, plogis) } # dim(or_pred_matrix2)
+    
 
     # compute MSE/loglik
     if(is.null(newdata)){ or_risk <- apply(or_pred_matrix1, 2, function(x){mean((L2[folds == validFold] - x)^2)}) } else { or_risk <- NULL }
@@ -195,11 +185,10 @@ one_hal_or_m2ts <- function(validFold, folds, L0, A0, L1, A1, L2, outcome_family
 
 
 # A helper function for fitting a single fold of OHAL on treatment. Can be used both for fitting cross-validated HAL and fitting HAL to full data.
-
 one_hal_ps_2ts <- function(validFold, folds, L0, A0, L1, A1, L2, Q2mod_coef_list, Q1mod_coef_list, # passed in from HAL OR
                            penalty_gamma = 1, penalty_threshold = 1e-5, newdata = NULL, lambda_seq = exp(seq(0, -13, length=2000)), tolg = 1e-5){
   n <- length(L2)
-  n_valid <- sum(validFold == folds) # 200 if V=5, n=1000
+  n_valid <- sum(validFold == folds) 
   n_train <- n - n_valid
 
   # if called during cross-validation
@@ -227,16 +216,16 @@ one_hal_ps_2ts <- function(validFold, folds, L0, A0, L1, A1, L2, Q2mod_coef_list
 
   basis_list <- enumerate_basis(x0_fit, NULL)
   x0_basis <- hal9001:::make_design_matrix(x0_fit, basis_list)  # dim: n_train * 800
-  copy_map <- hal9001:::make_copy_map(x0_basis) # length(copy_map) 800, output a list of numeric vectors indicating indices of basis functions that are identical in the training set
-  unique_columns <- as.numeric(names(copy_map)) # length(unique_columns) 800
+  copy_map <- hal9001:::make_copy_map(x0_basis) # 
+  unique_columns <- as.numeric(names(copy_map)) #
   # subset to non-duplicated columns
   x0_basis_fit <- x0_basis[, unique_columns] # dim(x0_basis_fit)
 
   # Q coefficients
   # !!! the number of coefs from Q should equal to the dimension of covariates included into the model
   or_coef_tmp0 <- abs(Q1mod_coef_list[-1])
-  penalty.factor <- or_coef_tmp0^(-penalty_gamma) # length(penalty.factor)= 451, could be different across different folds
-  penalty.factor[or_coef_tmp0 < penalty_threshold] <- Inf # sum(penalty.factor== Inf)=449
+  penalty.factor <- or_coef_tmp0^(-penalty_gamma) # 
+  penalty.factor[or_coef_tmp0 < penalty_threshold] <- Inf 
 
   if(!all(or_coef_tmp0 == 0)){
     # fit ohal
@@ -249,8 +238,8 @@ one_hal_ps_2ts <- function(validFold, folds, L0, A0, L1, A1, L2, Q2mod_coef_list
   new_x_basis0 <- hal9001:::make_design_matrix(new_x0, basis_list)
   new_x_basis0 <- as.matrix(new_x_basis0[, unique_columns])
 
-  ps_pred_matrix0 <- cbind(rep(1, ifelse(is.null(validFold), n, n_valid)), new_x_basis0) %*% rbind(ohal_lasso0$a0, ohal_lasso0$beta) # dim(ps_pred_matrix0)  n * length(lambda_seq)
-  ps_pred_matrix0 <- apply(ps_pred_matrix0, 2, plogis)  # dim(or_pred_matrix2)  n * length(lambda_seq)
+  ps_pred_matrix0 <- cbind(rep(1, ifelse(is.null(validFold), n, n_valid)), new_x_basis0) %*% rbind(ohal_lasso0$a0, ohal_lasso0$beta) 
+  ps_pred_matrix0 <- apply(ps_pred_matrix0, 2, plogis)  
   ps_pred_matrix0[ps_pred_matrix0 < tolg] <- tolg
   ps_pred_matrix0[ps_pred_matrix0 > 1 - tolg] <- 1 - tolg
 
@@ -262,9 +251,9 @@ one_hal_ps_2ts <- function(validFold, folds, L0, A0, L1, A1, L2, Q2mod_coef_list
   new_x1 = cbind(L0, L1)
 
   basis_list <- enumerate_basis(x1_fit, NULL)
-  x1_basis <- hal9001:::make_design_matrix(x1_fit, basis_list)  # dim: n_train * 800
-  copy_map <- hal9001:::make_copy_map(x1_basis) # length(copy_map) 800, output a list of numeric vectors indicating indices of basis functions that are identical in the training set
-  unique_columns <- as.numeric(names(copy_map)) # length(unique_columns) 800
+  x1_basis <- hal9001:::make_design_matrix(x1_fit, basis_list)  
+  copy_map <- hal9001:::make_copy_map(x1_basis) # output a list of numeric vectors indicating indices of basis functions that are identical in the training set
+  unique_columns <- as.numeric(names(copy_map)) # 
   # subset to non-duplicated columns
   x1_basis_fit <- x1_basis[, unique_columns] # dim(x1_basis_fit)
   x1_basis_fit = cbind(train_A0, x1_basis_fit)
@@ -272,8 +261,8 @@ one_hal_ps_2ts <- function(validFold, folds, L0, A0, L1, A1, L2, Q2mod_coef_list
   # Q coefficients
   # !!! the number of coefs from Q should equal to the dimension of covariates included into the model
   or_coef_tmp2 <- abs(Q2mod_coef_list[-2])
-  penalty.factor <- or_coef_tmp2^(-penalty_gamma) # length(penalty.factor)= 2388, could be different across different folds
-  penalty.factor[or_coef_tmp2 < penalty_threshold] <- Inf # sum(penalty.factor== Inf)=1918
+  penalty.factor <- or_coef_tmp2^(-penalty_gamma) # 
+  penalty.factor[or_coef_tmp2 < penalty_threshold] <- Inf # 
 
 
   if(!all(or_coef_tmp2 == 0)){
@@ -290,8 +279,8 @@ one_hal_ps_2ts <- function(validFold, folds, L0, A0, L1, A1, L2, Q2mod_coef_list
   new_x_basis1 <- hal9001:::make_design_matrix(new_x1, basis_list)
   new_x_basis1 <- as.matrix(new_x_basis1[, unique_columns])
 
-  ps_pred_matrix1 <- cbind(rep(1, ifelse(is.null(validFold), n, n_valid)), cbind(A0=abar[1],new_x_basis1)) %*% rbind(ohal_lasso1$a0, ohal_lasso1$beta) # dim(ps_pred_matrix0)  n * length(lambda_seq)
-  ps_pred_matrix1 <- apply(ps_pred_matrix1, 2, plogis)  # dim(or_pred_matrix2)  n * length(lambda_seq)
+  ps_pred_matrix1 <- cbind(rep(1, ifelse(is.null(validFold), n, n_valid)), cbind(A0=abar[1],new_x_basis1)) %*% rbind(ohal_lasso1$a0, ohal_lasso1$beta)da_seq)
+  ps_pred_matrix1 <- apply(ps_pred_matrix1, 2, plogis)  
 
   ps_pred_matrix1[ps_pred_matrix1 < tolg] <- tolg
   ps_pred_matrix1[ps_pred_matrix1 > 1 - tolg] <- 1 - tolg
